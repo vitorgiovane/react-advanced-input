8028735207bf74e897cb659390d1f53e201b778d
Vitor Giovane <vitorgpalves@gmail.com>
2020-11-15 02:18:05 -0300

    feat: adds custom limit for hours, minutes and seconds

--- demo/index.js
+++ demo/index.js
@@ -3,8 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TextField from '@material-ui/core/TextField';
 
-import TimeField from '../';
-//import TimeField from '../src';
+// import TimeField from '../';
+import TimeField from '../src';
 
 class App extends React.Component {
   constructor() {
@@ -25,11 +25,11 @@ class App extends React.Component {
     const timeSeconds = newTime.padEnd(8, this.state.timeSeconds.substr(5, 3));
     const timeSecondsCustomColon = timeSeconds.replace(/:/g, '-');
 
-    this.setState({time, timeSeconds, timeSecondsCustomColon});
+    this.setState({ time, timeSeconds, timeSecondsCustomColon });
   }
 
   render() {
-    const {time, timeSeconds, timeSecondsCustomColon} = this.state;
+    const { time, timeSeconds, timeSecondsCustomColon } = this.state;
 
     return (
       <section className="container">
@@ -46,6 +46,7 @@ class App extends React.Component {
               color: '#333',
               borderRadius: 3
             }}
+            maxHours={99}
           />
         </section>
         <h2>Show seconds:</h2>
@@ -62,6 +63,9 @@ class App extends React.Component {
               color: '#333',
               borderRadius: 3
             }}
+            maxHours={99}
+            maxMinutes={59}
+            maxSeconds={59}
           />
         </section>
         <h2>Custom colon:</h2>
@@ -79,22 +83,23 @@ class App extends React.Component {
               color: '#333',
               borderRadius: 3
             }}
+            maxHours={99}
           />
         </section>
         <h2>React Material-UI:</h2>
         <section>
-          <div style={{marginRight: 20}}>
+          <div style={{ marginRight: 20 }}>
             <TimeField
               showSeconds
               value={timeSeconds}
               onChange={this.onTimeChange}
-              style={{width: 88}}
+              style={{ width: 88 }}
               input={<TextField label="Name" value={timeSeconds} variant="outlined" />}
             />
           </div>
         </section>
       </section>
-    );
+    )
   }
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,53 @@
-import React, {ChangeEvent, CSSProperties, ReactElement} from 'react';
+import React, { ChangeEvent, CSSProperties, ReactElement } from 'react'
 
-const DEFAULT_COLON = ':';
-const DEFAULT_VALUE_SHORT = `00${DEFAULT_COLON}00`;
-const DEFAULT_VALUE_FULL = `00${DEFAULT_COLON}00${DEFAULT_COLON}00`;
+const DEFAULT_COLON = ':'
+const DEFAULT_VALUE_SHORT = `00${DEFAULT_COLON}00`
+const DEFAULT_VALUE_FULL = `00${DEFAULT_COLON}00${DEFAULT_COLON}00`
 
 export function isNumber<T>(value: T): boolean {
-  const number = Number(value);
-  return !isNaN(number) && String(value) === String(number);
+  const number = Number(value)
+  return !isNaN(number) && String(value) === String(number)
 }
 
 export function formatTimeItem(value?: string | number): string {
-  return `${value || ''}00`.substr(0, 2);
+  return `${value || ''}00`.substr(0, 2)
+}
+
+interface DecimalBlock {
+  newDecimalBlock: string
+  newCursorBlockPosition: number
+}
+
+function buildDecimalPlace(
+  oldDecimalBlock: string,
+  newDecimalBlock: string,
+  maxDecimalBlock: number,
+  newCursorBlockPosition: number
+): DecimalBlock {
+  if (!oldDecimalBlock) return { newDecimalBlock, newCursorBlockPosition }
+
+  const firstDigitOfMaxDecimalBlock = Number(maxDecimalBlock.toString()[0])
+  const secondDigitOfMaxDecimalBlock = Number(maxDecimalBlock.toString()[1])
+
+  const firstDigitOfOldDecimalBlock = Number(oldDecimalBlock[0])
+  const secondDigitOfOldDecimalBlock = Number(oldDecimalBlock[1])
+
+  const firstDigitOfNewDecimalBlock = Number(newDecimalBlock[0])
+  const secondDigitOfNewDecimalBlock = Number(newDecimalBlock[1])
+
+  if (firstDigitOfNewDecimalBlock > firstDigitOfMaxDecimalBlock) {
+    newDecimalBlock = oldDecimalBlock
+    newCursorBlockPosition -= 1
+  } else if (firstDigitOfNewDecimalBlock === firstDigitOfMaxDecimalBlock) {
+    if (firstDigitOfOldDecimalBlock === firstDigitOfMaxDecimalBlock && secondDigitOfNewDecimalBlock > secondDigitOfMaxDecimalBlock) {
+      newDecimalBlock = `${firstDigitOfMaxDecimalBlock}${secondDigitOfOldDecimalBlock}`
+      newCursorBlockPosition -= 2
+    } else if (secondDigitOfNewDecimalBlock > secondDigitOfMaxDecimalBlock) {
+      newDecimalBlock = maxDecimalBlock.toString()
+    }
+  }
+
+  return { newDecimalBlock, newCursorBlockPosition }
 }
 
 export function validateTimeAndCursor(
@@ -18,90 +55,106 @@ export function validateTimeAndCursor(
   value = '',
   defaultValue = '',
   colon = DEFAULT_COLON,
-  cursorPosition = 0
+  cursorPosition = 0,
+  maxHours = 23,
+  maxMinutes = 59,
+  maxSeconds = 59
 ): [string, number] {
-  const [oldH, oldM, oldS] = defaultValue.split(colon);
+  const [oldHours, oldMinutes, oldSeconds] = defaultValue.split(colon)
 
-  let newCursorPosition = Number(cursorPosition);
-  let [newH, newM, newS] = String(value).split(colon);
+  let newCursorPosition = Number(cursorPosition)
+  let [newHours, newMinutes, newSeconds] = String(value).split(colon)
 
-  newH = formatTimeItem(newH);
-  if (Number(newH[0]) > 2) {
-    newH = oldH;
-    newCursorPosition -= 1;
-  } else if (Number(newH[0]) === 2) {
-    if (Number(oldH[0]) === 2 && Number(newH[1]) > 3) {
-      newH = `2${oldH[1]}`;
-      newCursorPosition -= 2;
-    } else if (Number(newH[1]) > 3) {
-      newH = '23';
-    }
-  }
+  newHours = formatTimeItem(newHours)
+  const {
+    newDecimalBlock: newHoursBlock,
+    newCursorBlockPosition: newCursorHoursPosition
+  } = buildDecimalPlace(oldHours, newHours, maxHours, newCursorPosition)
+  newHours = newHoursBlock
+  newCursorPosition = newCursorHoursPosition
 
-  newM = formatTimeItem(newM);
-  if (Number(newM[0]) > 5) {
-    newM = oldM;
-    newCursorPosition -= 1;
-  }
+  newMinutes = formatTimeItem(newMinutes)
+  const {
+    newDecimalBlock: newMinutesBlock,
+    newCursorBlockPosition: newCursorMinutesPosition
+  } = buildDecimalPlace(oldMinutes, newMinutes, maxMinutes, newCursorPosition)
+  newMinutes = newMinutesBlock
+  newCursorPosition = newCursorMinutesPosition
 
   if (showSeconds) {
-    newS = formatTimeItem(newS);
-    if (Number(newS[0]) > 5) {
-      newS = oldS;
-      newCursorPosition -= 1;
-    }
+    newSeconds = formatTimeItem(newSeconds)
+    const {
+      newDecimalBlock: newSecondsBlock,
+      newCursorBlockPosition: newCursorSecondsPosition
+    } = buildDecimalPlace(oldSeconds, newSeconds, maxSeconds, newCursorPosition)
+    newSeconds = newSecondsBlock
+    newCursorPosition = newCursorSecondsPosition
   }
 
-  const validatedValue = showSeconds ? `${newH}${colon}${newM}${colon}${newS}` : `${newH}${colon}${newM}`;
+  const validatedValue = showSeconds
+    ? `${newHours}${colon}${newMinutes}${colon}${newSeconds}`
+    : `${newHours}${colon}${newMinutes}`
 
-  return [validatedValue, newCursorPosition];
+  return [validatedValue, newCursorPosition]
 }
 
-type onChangeType = (event: ChangeEvent<HTMLInputElement>, value: string) => void;
+type onChangeType = (event: ChangeEvent<HTMLInputElement>, value: string) => void
 
 interface Props {
-  value?: string;
-  onChange?: onChangeType;
-  showSeconds?: boolean;
-  input: ReactElement | null;
-  inputRef?: () => HTMLInputElement | null;
-  colon?: string;
-  style?: CSSProperties | {};
+  value?: string
+  onChange?: onChangeType
+  showSeconds?: boolean
+  input: ReactElement | null
+  inputRef?: () => HTMLInputElement | null
+  colon?: string
+  style?: CSSProperties | {}
+  maxHours?: number
+  maxMinutes?: number
+  maxSeconds?: number
 }
 
 interface State {
-  value: string;
-  _colon: string;
-  _defaultValue: string;
-  _showSeconds: boolean;
-  _maxLength: number;
+  value: string
+  _colon: string
+  _defaultValue: string
+  _showSeconds: boolean
+  _maxLength: number
+  _maxHours?: number
+  _maxMinutes?: number
+  _maxSeconds?: number
 }
 
 export default class TimeField extends React.Component<Props, State> {
-  static defaultProps: Props = {
-    showSeconds: false,
-    input: null,
-    style: {},
-    colon: DEFAULT_COLON
-  };
-
   constructor(props: Props) {
-    super(props);
+    super(props)
 
-    const _showSeconds = Boolean(props.showSeconds);
-    const _defaultValue = _showSeconds ? DEFAULT_VALUE_FULL : DEFAULT_VALUE_SHORT;
-    const _colon = props.colon && props.colon.length === 1 ? props.colon : DEFAULT_COLON;
-    const [validatedTime] = validateTimeAndCursor(_showSeconds, this.props.value, _defaultValue, _colon);
+    const _showSeconds = Boolean(props.showSeconds)
+    const _defaultValue = _showSeconds ? DEFAULT_VALUE_FULL : DEFAULT_VALUE_SHORT
+    const _colon = props.colon && props.colon.length === 1 ? props.colon : DEFAULT_COLON
+    const { maxHours: _maxHours, maxMinutes: _maxMinutes, maxSeconds: _maxSeconds } = props
+    const [validatedTime] = validateTimeAndCursor(
+      _showSeconds,
+      this.props.value,
+      _defaultValue,
+      _colon,
+      0,
+      _maxHours,
+      _maxMinutes,
+      _maxSeconds
+    )
 
     this.state = {
       value: validatedTime,
       _colon,
       _showSeconds,
       _defaultValue,
-      _maxLength: _defaultValue.length
-    };
+      _maxLength: _defaultValue.length,
+      _maxHours,
+      _maxMinutes,
+      _maxSeconds
+    }
 
-    this.onInputChange = this.onInputChange.bind(this);
+    this.onInputChange = this.onInputChange.bind(this)
   }
 
   componentDidUpdate(prevProps: Props): void {
@@ -110,71 +163,76 @@ export default class TimeField extends React.Component<Props, State> {
         this.state._showSeconds,
         this.props.value,
         this.state._defaultValue,
-        this.state._colon
-      );
+        this.state._colon,
+        0,
+        this.props.maxHours,
+        this.props.maxMinutes,
+        this.props.maxSeconds
+      )
+
       this.setState({
         value: validatedTime
-      });
+      })
     }
   }
 
   onInputChange(event: ChangeEvent<HTMLInputElement>, callback: onChangeType): void {
-    const oldValue = this.state.value;
-    const inputEl = event.target;
-    const inputValue = inputEl.value;
-    const position = inputEl.selectionEnd || 0;
-    const isTyped = inputValue.length > oldValue.length;
-    const cursorCharacter = inputValue[position - 1];
-    const addedCharacter = isTyped ? cursorCharacter : null;
-    const removedCharacter = isTyped ? null : oldValue[position];
-    const replacedSingleCharacter = inputValue.length === oldValue.length ? oldValue[position - 1] : null;
-    const colon = this.state._colon;
+    const oldValue = this.state.value
+    const inputEl = event.target
+    const inputValue = inputEl.value
+    const position = inputEl.selectionEnd || 0
+    const isTyped = inputValue.length > oldValue.length
+    const cursorCharacter = inputValue[position - 1]
+    const addedCharacter = isTyped ? cursorCharacter : null
+    const removedCharacter = isTyped ? null : oldValue[position]
+    const replacedSingleCharacter = inputValue.length === oldValue.length ? oldValue[position - 1] : null
+    const colon = this.state._colon
 
-    let newValue = oldValue;
-    let newPosition = position;
+    let newValue = oldValue
+    let newPosition = position
 
     if (addedCharacter !== null) {
       if (position > this.state._maxLength) {
-        newPosition = this.state._maxLength;
+        newPosition = this.state._maxLength
       } else if ((position === 3 || position === 6) && addedCharacter === colon) {
-        newValue = `${inputValue.substr(0, position - 1)}${colon}${inputValue.substr(position + 1)}`;
+        newValue = `${inputValue.substr(0, position - 1)}${colon}${inputValue.substr(position + 1)}`
       } else if ((position === 3 || position === 6) && isNumber(addedCharacter)) {
-        newValue = `${inputValue.substr(0, position - 1)}${colon}${addedCharacter}${inputValue.substr(position + 2)}`;
-        newPosition = position + 1;
+        newValue = `${inputValue.substr(0, position - 1)}${colon}${addedCharacter}${inputValue.substr(position + 2)}`
+        newPosition = position + 1
       } else if (isNumber(addedCharacter)) {
         // user typed a number
-        newValue = inputValue.substr(0, position - 1) + addedCharacter + inputValue.substr(position + 1);
+        newValue = inputValue.substr(0, position - 1) + addedCharacter + inputValue.substr(position + 1)
         if (position === 2 || position === 5) {
-          newPosition = position + 1;
+          newPosition = position + 1
         }
       } else {
         // if user typed NOT a number, then keep old value & position
-        newPosition = position - 1;
+        newPosition = position - 1
       }
     } else if (replacedSingleCharacter !== null) {
       // user replaced only a single character
       if (isNumber(cursorCharacter)) {
         if (position - 1 === 2 || position - 1 === 5) {
-          newValue = `${inputValue.substr(0, position - 1)}${colon}${inputValue.substr(position)}`;
+          newValue = `${inputValue.substr(0, position - 1)}${colon}${inputValue.substr(position)}`
         } else {
-          newValue = inputValue;
+          newValue = inputValue
         }
       } else {
         // user replaced a number on some non-number character
-        newValue = oldValue;
-        newPosition = position - 1;
+        newValue = oldValue
+        newPosition = position - 1
       }
     } else if (typeof cursorCharacter !== 'undefined' && cursorCharacter !== colon && !isNumber(cursorCharacter)) {
       // set of characters replaced by non-number
-      newValue = oldValue;
-      newPosition = position - 1;
+      newValue = oldValue
+      newPosition = position - 1
     } else if (removedCharacter !== null) {
       if ((position === 2 || position === 5) && removedCharacter === colon) {
-        newValue = `${inputValue.substr(0, position - 1)}0${colon}${inputValue.substr(position)}`;
-        newPosition = position - 1;
+        newValue = `${inputValue.substr(0, position - 1)}0${colon}${inputValue.substr(position)}`
+        newPosition = position - 1
       } else {
         // user removed a number
-        newValue = `${inputValue.substr(0, position)}0${inputValue.substr(position)}`;
+        newValue = `${inputValue.substr(0, position)}0${inputValue.substr(position)}`
       }
     }
 
@@ -183,23 +241,26 @@ export default class TimeField extends React.Component<Props, State> {
       newValue,
       oldValue,
       colon,
-      newPosition
-    );
+      newPosition,
+      this.props.maxHours,
+      this.props.maxMinutes,
+      this.props.maxSeconds
+    )
 
-    this.setState({value: validatedTime}, () => {
-      inputEl.selectionStart = validatedCursorPosition;
-      inputEl.selectionEnd = validatedCursorPosition;
-      callback(event, validatedTime);
-    });
+    this.setState({ value: validatedTime }, () => {
+      inputEl.selectionStart = validatedCursorPosition
+      inputEl.selectionEnd = validatedCursorPosition
+      callback(event, validatedTime)
+    })
 
-    event.persist();
+    event.persist()
   }
 
   render(): ReactElement {
-    const {value} = this.state;
-    const {onChange, style, showSeconds, input, inputRef, colon, ...props} = this.props; //eslint-disable-line no-unused-vars
+    const { value } = this.state
+    const { onChange, style, showSeconds, input, inputRef, colon, maxHours, maxMinutes, maxSeconds, ...props } = this.props //eslint-disable-line no-unused-vars
     const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) =>
-      this.onInputChange(event, (e: ChangeEvent<HTMLInputElement>, v: string) => onChange && onChange(e, v));
+      this.onInputChange(event, (e: ChangeEvent<HTMLInputElement>, v: string) => onChange && onChange(e, v))
 
     if (input) {
       return React.cloneElement(input, {
@@ -207,7 +268,7 @@ export default class TimeField extends React.Component<Props, State> {
         value,
         style,
         onChange: onChangeHandler
-      });
+      })
     }
 
     return (
@@ -217,8 +278,8 @@ export default class TimeField extends React.Component<Props, State> {
         ref={inputRef}
         value={value}
         onChange={onChangeHandler}
-        style={{width: showSeconds ? 54 : 35, ...style}}
+        style={{ width: showSeconds ? 54 : 35, ...style }}
       />
-    );
+    )
   }
 }
